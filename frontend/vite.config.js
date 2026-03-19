cf1ad7e63097fc95bc105237a6c188078f05155f
Ung Hoàng Khang <origami362@gmail.com>
2026-03-19 20:21:28 +0700

    Revert "Feature/huy tour"

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,29 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (!id.includes('node_modules')) {
-            return undefined
-          }
-
-          if (id.includes('react-icons')) {
-            return 'vendor-react-icons'
-          }
-
-          if (id.includes('recharts')) {
-            return 'vendor-recharts'
-          }
-
-          if (id.includes('slick-carousel') || id.includes('react-slick')) {
-            return 'vendor-slick'
-          }
-
-          return 'vendor-misc'
-        },
-      },
-    },
-  },
 })
